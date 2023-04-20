e1350c25dd99781e61ec9e52d8907fc1f3371486
roberthsu2003 <roberthsu2003@gmail.com>
2023-04-20 11:19:53 +0800

    modify

---
+++ main.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+
+def main()->None:
+    window = Window()
+    window.title("第一次視窗")
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
